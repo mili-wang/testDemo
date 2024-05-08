957c7e829c8cc2ffe591cd13196c2565055d9295
WangChang <676490247@qq.com>
2024-05-08 14:01:12 +0800

    打包

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,10 +2,11 @@ import axios from "axios"
 import { getToken } from "./auth";
 import { showSuccessToast } from 'vant';
 
-console.log(import.meta);
+console.log(import.meta, import.meta.env.VITE_BASE_API);
 // 创建axios实例
 const instance = axios.create({
-  baseURL: '/', // 设置默认的 API 地址，url = baseURL + url（使用proxy代理时此处可不写）
+  baseURL: import.meta.env.VITE_BASE_API,
+//   baseURL: '/', // 设置默认的 API 地址，url = baseURL + url（使用proxy代理时此处可不写）
   timeout: 5000,						  // 设置请求超时时间
   headers: { 'systemType': 'IOS' }        // 设置请求header，可以自定义属性
 })
